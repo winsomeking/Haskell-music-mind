d8ddb2a49e1c56ea80b04aad6e1231f1c565aa7b
winsomeking <albertmelb@hotmail.com>
2013-09-21 13:09:35 +1000

    Change the definition of GameState, added new functions.

--- MusicMind.hs
+++ MusicMind.hs
@@ -15,39 +15,34 @@ import Debug.Trace
 import Data.List
 	
 -- |fst GameState is a list of all previous guesses.snd GameState is the result of previous guess.	
-type GameState = ( [[String]] , (Int,Int,Int)) 	
+-- type GameState = ( [[String]] , (Int,Int,Int)) 	
+type GameState = [[String]]
 
-
--- |Initial guess is randomly chosen.	
--- |GameState (0,0,0) means the first guess.
+-- |Initial guess is chosen by the developer.	
 
 initialGuess :: ([String],GameState)
-initialGuess = (["A1","B1","C2"],(["A1","B1","C2"]:[],(0,0,0)))
+initialGuess = (["A1","B1","C2"],targetsList)
+           where targetsList = allCombinations ["A","B","C","D","E","F","G"] (1,2,3)
 
 -- |Debug code.
 nextGuess = observe "Arguments for nextGuess' " nextGuess'
 
-nextGuess' :: ([String],GameState) -> (Int,Int,Int) -> ([String],GameState)
+nextGuess :: ([String],GameState) -> (Int,Int,Int) -> ([String],GameState)
 -- |Debug code. Trace the input value of arguments
-nextGuess' record result | trace ("-- Debug: nextGuess' " ++ show record ++ " " ++ show result) False = undefined
+nextGuess record result | trace ("-- Debug: nextGuess' " ++ show record ++ " " ++ show result) False = undefined
 -- |FIX ME: Change the newGuess object.
-nextGuess' record result = (newGuess,(newGuess:(fst.snd) record, result) ) 
+-- Result cannot be (3,0,0), since if successfully guessed the target, then this method will not be called.
+nextGuess record result = (newGuess, newGameState) 
            where newGuess = ["A1","B1","C3"]
+                 newGameState = updateGameState record result
 
 -- |Helper functions. |Private
+ 
 
--- |Generate all the possible combinations based on the result and gameState. 
--- Previous used combinations will not be included in this list.
-
--- possibleTargetsUnderResult:: (Int,Int,Int) -> GameState -> [[String]]
--- possibleTargetsUnderResult result gameState =   
-
+-- | Get all the 1330 possible targets.
 allCombinations:: [String] -> (Int,Int,Int) -> [[String]]
 --allCombinations strList intTuple | trace ("-- Debug: allCominations " ++ show strList ++ " " ++ show intTuple) False = undefined
-allCombinations strList intTuple = removeDuplicates combinations
-	           --[ xs | xs<-combinations, ys<-combinations,(all ( `elem` ys) xs ) == False ]
-	           --[ xs | xs<-combinations, ys<-combinations,(all ( `elem` ys) xs ) == False ]
-	           --filter ((all (`elem` ys) xs ) == True) combinations             
+allCombinations strList intTuple = removeDuplicates combinations           
          where intList = (first intTuple):(second intTuple):(third intTuple):[]
                first (x,_,_) = x
                second(_,y,_) = y
@@ -55,6 +50,7 @@ allCombinations strList intTuple = removeDuplicates combinations
                tempList = [ (x ++ (show n))| x <- strList,n <- intList]
                combinations =  [a:b:c:[] | a<-tempList,b<-tempList,c<-tempList, a/=b,b/=c, a/=c ]
 
+-- | Remove duplicate targets.For example ["A1","B2","C3"] and  ["A1","C3","B2"] 
 removeDuplicates :: [[String]] ->[[String]]
 -- removeDuplicates originalList | trace ("--Debug: removeDuplicates: " ++ show originalList) False = undefined
 removeDuplicates [] = []
@@ -63,9 +59,36 @@ removeDuplicates originalList = if any ( `elem`  (permutations.head) originalLis
                                   else  (head originalList):((removeDuplicates.tail) originalList)
 
 
+
+-- | Remove those targets that are inconsistent with the result from the targets list.
+-- Input GameState and result, output a new GameState.
+updateGameState:: ([String],GameState) -> (Int,Int,Int) -> GameState
+updateGameState record result = if checkConsistency (fst record) (head GameState) result  
+                                   then (head GameState):(updateGameState (fst record,tail GameState) result):[]
+	                               else (updateGameState (fst record,tail GameState) result):[]
+	
+	
+checkConsistency:: [String] -> [String] -> (Int,Int,Int) -> Bool
+checkConsistency lastGuess element lastResult = if (pitch,note,oct) == lastResult
+	                								then True
+	                                                else False
+	           where pitch = 3 - (length  distinctElement)
+		             note  = if (length distinctElement /= 0) then   else 0 
+			         oct   = if (length distinctElement /= 0) then   else 0 
+                     distinctElement    = element\\lastResult     
+                     distinctLastResult = lastResult\\element
+
+
+
+
+ 	
+		
+		
+
+
+
 testMethod::IO ()
 testMethod = putStrLn $ (fst initialGuess) !! 0 ++ (fst initialGuess) !! 1 ++ (fst initialGuess) !! 2 
-
 
 
 --nextGuess :: ([String],GameState) -> (Int,Int,Int) -> ([String],GameState)	
